71c5e13d6df1800a7c25531f4276ecb3ff634c1b
monicah79 <monniewanjema@gmail.com>
2023-04-27 19:53:20 +0300

    Processing data in modes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  belongs_to :post
+  belongs_to :user, class_name: 'User'
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments_counter + 1)
+  end
 end
--- db/migrate/20230421093600_add_foreign_keys_to_comments.rb
+++ db/migrate/20230421093600_add_foreign_keys_to_comments.rb
@@ -4,6 +4,6 @@
 class AddForeignKeysToComments < ActiveRecord::Migration[7.0]
   def change
     add_foreign_key :comments, :users, column: :author_id
-    add_index :comments, :user_id
+    add_index :comments, :author_id
   end
 end
